4d9f5b6c8349d88bdf90015b3464af45391774ca
vecin2 <vecin2@gmail.com>
2016-12-21 20:09:39 +0000

    refactoring tests and code

--- ruby/lib/roman_number.rb
+++ ruby/lib/roman_number.rb
@@ -1,45 +1,45 @@
 class RomanNumber
-  @@arabics#
+  @@arabics
+
   def initialize
     @@arabics = {1=>'I', 5=>'V',10=>'X',50=>'L',100=>'C',500=>'D',1000=>'M'}
     @@arabics.default=''
   end
 
+
   def to_roman(arabic)
     result =''
-
-    if @@arabics[arabic]!=''
-      return @@arabics[arabic]
-    end
     expand_arabic(arabic).each { |current_arabic|
-      while(current_arabic>0)
-        if substraction_notation(current_arabic)!=''
-          arabic_to_decrease=current_arabic
-          roman_symbol= substraction_notation(current_arabic)
-        else
-          arabic_to_decrease= get_exact_arabic(current_arabic)
-          roman_symbol=@@arabics[arabic_to_decrease]
-        end
-        result += roman_symbol
-        current_arabic = current_arabic - arabic_to_decrease
-      end
+      result += compute_roman(current_arabic)
     }
     return result
   end
 
+
   def expand_arabic(arabic)
-
     expanded_arabic=Array.new
-
     units =  1
     while (arabic>=units)
       units *= 10
       current_value =arabic%units
-      expanded_arabic.unshift(current_value)
+      if current_value > 0
+        expanded_arabic.unshift(current_value)
+      end
       arabic -= current_value
     end
     return expanded_arabic
   end
+ 
+
+  def compute_roman(current_arabic)
+    if substraction_notation(current_arabic)!=''
+      roman_symbol= substraction_notation(current_arabic)
+    else
+      roman_symbol= addition_notation(current_arabic)
+    end
+  end
+
+
   def substraction_notation(current_arabic)
     roman_symbol=''
     if @@arabics[current_arabic+1]!=''
@@ -49,19 +49,33 @@ class RomanNumber
     elsif @@arabics[current_arabic+100]!=''
       roman_symbol=combine_roman_symbols(100,current_arabic+100)
     end
+
     return roman_symbol
   end
+
+
+  def addition_notation(current_arabic)
+    roman_symbol=''
+    while(current_arabic>0)
+      arabic_to_decrease= get_next_arabic(current_arabic)
+      roman_symbol+=@@arabics[arabic_to_decrease]
+      current_arabic = current_arabic - arabic_to_decrease
+    end
+    roman_symbol
+  end
+
+
   def combine_roman_symbols(prefix,exact_arabic) 
     return @@arabics[prefix]+@@arabics[exact_arabic]
   end
 
 
-  def get_exact_arabic(arabic)
+  def get_next_arabic(arabic)
     @@arabics.keys.reverse_each do |key|
       if key <= arabic
         return key
       end
-
     end
   end
+
 end
--- ruby/spec/roman_number_spec.rb
+++ ruby/spec/roman_number_spec.rb
@@ -1,61 +1,25 @@
 require_relative '../lib/roman_number.rb'
 
 describe RomanNumber do
-  it "displays number from one to ten" do
-    test(1,'I')
-    test(2,'II')
-    test(3,'III')
-    test(4,'IV')
-    test(5,'V')
-    test(6,'VI')
-    test(7,'VII')
-    test(8,'VIII')
-    test(9,'IX')
-    test(10,'X')
-    test(11,'XI')
-    test(12,'XII')
-    test(13,'XIII')
-    test(14,'XIV')
-    test(15,'XV')
-    test(16,'XVI')
-    test(17,'XVII')
-    test(18,'XVIII')
-    test(19,'XIX')
-    test(20,'XX')
-    test(21,'XXI')
-    test(25,'XXV')
-    test(30,'XXX')
-    test(40,'XL')
-    test(50,'L')
-    test(88,'LXXXVIII')
-    test(89,'LXXXIX')
-    test(99,'XCIX')
-    test(100,'C')
-    test(90,'XC')
-    test(109,'CIX')
-    test(138,'CXXXVIII')
-    test(140,'CXL')
-    test(189,'CLXXXIX')
-    test(390,'CCCXC')
-    test(390,'CCCXC')
-    test(399,'CCCXCIX')
-    test(960,'CMLX')
-    test(4999,'MMMMCMXCIX')
+  @tests ={
+    1=>'I',2=>'II', 3=>'III', 4=>'IV', 5=>'V',
+    6=>'VI', 7=>'VII', 8=>'VIII', 9=>'IX', 10=>'X',
+    11=>'XI', 12=>'XII', 13=>'XIII', 14=>'XIV', 15=>'XV',
+    16=>'XVI', 17=>'XVII', 18=>'XVIII', 19=>'XIX', 20=>'XX',
+    21=>'XXI', 25=>'XXV', 30=>'XXX', 40=>'XL', 50=>'L',
+    88=>'LXXXVIII', 89=>'LXXXIX', 99=>'XCIX', 100=>'C',
+    90=>'XC', 109=>'CIX', 138=>'CXXXVIII', 140=>'CXL', 189=>'CLXXXIX',
+    390=>'CCCXC', 399=>'CCCXCIX', 960=>'CMLX',
+    4999=>'MMMMCMXCIX'
+  }
+  @tests.each do |arabic, roman|
+    it "From arabic '"+arabic.to_s+"' returns roman '"+roman+"'" do
+      test(arabic, roman)
+    end
   end
 
   def test(arabic, roman)
     romanNumber = RomanNumber.new
     expect(romanNumber.to_roman(arabic)).to eq(roman)
   end
-
-  it "expands the arabic number" do
-    romanNumber = RomanNumber.new
-    result=[9]
-    expect(romanNumber.expand_arabic(9)).to eq(result)
-    result=[90,9]
-    expect(romanNumber.expand_arabic(99)).to eq(result)
-    result=[10,1]
-    expect(romanNumber.expand_arabic(11)).to eq(result)
-  end
-
 end
